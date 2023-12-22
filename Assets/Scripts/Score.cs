4b92f79e66ba0e97494c47cea8268aa119c7d81c
HanchGG <emil.rakhimov.designer@gmail.com>
2023-12-22 19:03:44 +0300

    Second commit
    nothing

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    private float Timer;
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Raketa")
+        {
+            ScoreManager.score+=1;
+            Destroy(gameObject);
+        }
+    }
+    private void Update()
+    {
+        Timer += Time.deltaTime;
+        if (Timer > 6)
+        {
+            Destroy(gameObject);    
+        }
+    }
+}
